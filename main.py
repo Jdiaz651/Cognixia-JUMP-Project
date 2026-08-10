7a89065a8361d7782b5a6ffbeae204224157d3a8
Jaime Diaz <Jdiaz651@outlook.com>
2026-08-10 11:15:03 -0400

    Start prompt made

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the bank")
+    Username = input("Please enter your username:")
+    Password = input("Please enter you password:")
+
+    print("Your username and password is:", Username, Password)
+
+if __name__ == "__main__":
+    main()
